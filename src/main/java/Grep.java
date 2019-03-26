c47c09b8a435e7370dd039582aa2dba1faa18649
Alexandr Kobyzhev <kam-2000@mail.ru>
2019-03-26 20:07:19 +0300

    Добавление Pattern

--- src/main/java/Grep.java
+++ src/main/java/Grep.java
@@ -1,21 +1,33 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Grep{
 
     private String word, inputName;
     private boolean ignore;
 
-    public Grep(String regex, String inputFile, boolean ignoreWordCase) {
+    public Grep(String regex, String inputFile) {
         word = regex;
         inputName = inputFile;
-        ignore = ignoreWordCase;
     }
 
-    public List<String> finder() throws IOException {
+    public List<String> r(String word) throws IOException {
+        return finder(word, false);
+    }
+
+    public List<String> v(String word) throws IOException {
+        return finder(word, true);
+    }
+
+    public List<String> finder(String word, boolean ignore) throws IOException {
         BufferedReader buffer = new BufferedReader(new FileReader(new File(inputName)));
         String line = buffer.readLine();
         List<String> answer = new ArrayList<String>();
+        Pattern p = Pattern.compile(".*");
+        Matcher m = p.matcher(line);
+        if (m.matches())
     }
 }
